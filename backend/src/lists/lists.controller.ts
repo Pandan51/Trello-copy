fc78acccfea73fdba4622c62219bec251994cbc3
Daniel BLAHOVEC <dan792007@gmail.com>
2026-05-01 00:12:07 +0200

    adjusted position property from number to string in task DTOs

--- backend/src/lists/lists.controller.ts
+++ backend/src/lists/lists.controller.ts
@@ -1,46 +1,49 @@
 import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  Patch,
-  Post,
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Param,
+    Patch,
+    Post,
 } from '@nestjs/common';
-import { PartialType } from '@nestjs/mapped-types';
-import { ListsService } from './lists.service';
+import {PartialType} from '@nestjs/mapped-types';
+import {ListsService} from './lists.service';
 
 class CreateTaskListDto {
-  id: string;
-  title: string;
+    id: string;
+    title: string;
 }
 
-class UpdateTaskListDto extends PartialType(CreateTaskListDto) {}
+class UpdateTaskListDto extends PartialType(CreateTaskListDto) {
+}
 
 @Controller('lists')
 export class ListsController {
-  constructor(private listsService: ListsService) {}
+    constructor(private listsService: ListsService) {
+    }
 
-  @Get()
-  async getAllLists() {
-    return this.listsService.getAllLists();
-  }
+    @Get()
+    async getAllLists() {
+        return this.listsService.getAllLists();
+    }
 
-  @Post()
-  async createList(@Body() body: { title: string }) {
-    // Hand the incoming data to the Chef
-    return this.listsService.createList(body.title);
-  }
+    @Post()
+    async createList(@Body() body: { title: string }) {
+        // Hand the incoming data to the Chef
+        return this.listsService.createList(body.title);
+    }
 
-  @Delete('/:id')
-  async deleteList(@Param('id') id: string): Promise<CreateTaskListDto> {
-    return this.listsService.deleteList(id);
-  }
+    @Delete('/:id')
+    async deleteList(@Param('id') id: string): Promise<CreateTaskListDto> {
+        return this.listsService.deleteList(id);
+    }
 
-  @Patch('/:id')
-  async patchList(
-      @Param('id') id: string,
-      @Body() sentList: UpdateTaskListDto): Promise<any> {
-    return this.listsService.patchList(id, sentList);
-  }
+    @Patch('/:id')
+    async patchList(
+        @Param('id') id: string,
+        @Body() sentList: UpdateTaskListDto,
+    ): Promise<any> {
+        return this.listsService.patchList(id, sentList);
+    }
 }
